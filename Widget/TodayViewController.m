044393088282b7e4f9367f2323da3639173f20db
Sachin Patel <me@gizmosachin.com>
2014-09-20 17:50:51 -0700

    Auto Layout to support iPhone 6, shows floor and distance count.

--- Widget/TodayViewController.m
+++ Widget/TodayViewController.m
@@ -7,20 +7,24 @@
 //
 
 #import "TodayViewController.h"
-#import "DayCollectionViewCell.h"
 
 #import <CoreMotion/CoreMotion.h>
 #import <NotificationCenter/NotificationCenter.h>
 #import <QuartzCore/QuartzCore.h>
 
-@interface TodayViewController () <NCWidgetProviding, UICollectionViewDataSource, UICollectionViewDelegate>
+@interface TodayViewController () <NCWidgetProviding>
+
+@property (nonatomic, weak) IBOutlet UILabel *stepCountLabel;
+@property (nonatomic, weak) IBOutlet UILabel *mileCountLabel;
+@property (nonatomic, weak) IBOutlet UILabel *floorCountLabel;
+
+@property (nonatomic, weak) IBOutlet UIProgressView *todayProgressView;
+
+@property (nonatomic) NSInteger stepCount;
+@property (nonatomic) CGFloat mileCount;
+@property (nonatomic) NSInteger floorCount;
 
 @property (nonatomic, strong) NSMutableArray *lastWeek;
-@property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
-@property (nonatomic, weak) IBOutlet UILabel *stepCountLabel;
-@property (nonatomic, weak) IBOutlet UILabel *goalCountLabel;
-@property (nonatomic, weak) IBOutlet UIProgressView *todayProgressView;
-@property (nonatomic) NSInteger stepCount;
 @property (nonatomic, strong) CMPedometer *pedometer;
 @end
 
@@ -28,40 +32,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setPreferredContentSize:CGSizeMake(320.0, 100.0)];
+    [self setPreferredContentSize:CGSizeMake(320.0, 110.0f)];
     
-    self.lastWeek = [[NSMutableArray alloc] initWithCapacity:7];
-    for (NSInteger i = 0; i < 7; i++) {
-        self.lastWeek[i] = @(0);
-    }
+    self.todayProgressView.layer.cornerRadius = 5.0;
+    self.todayProgressView.layer.masksToBounds = YES;
     
-    CGAffineTransform transform = CGAffineTransformMakeScale(1.0f, 5.0f);
-    _todayProgressView.transform = transform;
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    _stepCount = [[[NSUserDefaults standardUserDefaults] valueForKey:@"stepCount"] integerValue];
+    [self updateLabels];
     
     if ([CMPedometer isStepCountingAvailable]) {
         _pedometer = [CMPedometer new];
         
         [_pedometer queryPedometerDataFromDate:[self midnightForDate:[NSDate date]] toDate:[NSDate date] withHandler:^(CMPedometerData *data, NSError *error) {
             _stepCount = data.numberOfSteps.integerValue;
-            [self updateSteps];
+            _mileCount = data.distance.floatValue * 0.000621371; // convert meters to miles
+            _floorCount = data.floorsAscended.integerValue;
+            
+            [self updateLabels];
             
             [_pedometer startPedometerUpdatesFromDate:[NSDate date] withHandler:^(CMPedometerData *data, NSError *error) {
                 _stepCount += data.numberOfSteps.integerValue;
-                [self updateSteps];
+                _mileCount += data.distance.floatValue * 0.000621371; // convert meters to miles
+                _floorCount += data.floorsAscended.integerValue;
+                
+                [self updateLabels];
             }];
         }];
-        
-        for (int i = 7; i > 1; i--) {
-            NSDate *startDate = [NSDate dateWithTimeIntervalSinceNow: -(60.0f*60.0f*24.0f*i)];
-            NSDate *endDate = [NSDate dateWithTimeIntervalSinceNow: -(60.0f*60.0f*24.0f*(i-1))];
-            
-            [_pedometer queryPedometerDataFromDate:[self midnightForDate:startDate] toDate:[self midnightForDate:endDate] withHandler:^(CMPedometerData *data, NSError *error) {
-                
-                _lastWeek[i] = data.numberOfSteps;
-                [self.collectionView reloadData];
-                
-            }];
-        }
     }
 }
 
@@ -76,28 +73,9 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Collection View Data Source
-
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return 1;
-}
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return 7;
-}
-
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    DayCollectionViewCell *cell = (DayCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:@"DayCollectionViewCell" forIndexPath:indexPath];
-    if (_lastWeek[indexPath.row] != nil && [_lastWeek[indexPath.row] isKindOfClass:[NSNumber class]]) {
-        CGFloat percent = [_lastWeek[indexPath.row] floatValue] / 10000.0f;
-        [cell setPercent:percent];
-    }
-    return cell;
-}
-
 #pragma mark - Core Motion
 
-- (void)updateSteps {
+- (void)updateLabels {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         dispatch_async(dispatch_get_main_queue(), ^{
             if (_stepCount < 10000) {
@@ -111,17 +89,20 @@
                 _stepCountLabel.text = [numberFormatter stringForObjectValue:number];
             }
             
+            _mileCountLabel.text = [NSString stringWithFormat:@"%.2f mile%@", _mileCount, _mileCount != 1.0 ? @"s" : @""];
+            _floorCountLabel.text = [NSString stringWithFormat:@"%ld floor%@", _floorCount, _floorCount != 1 ? @"s" : @""];
+            
             CGFloat percent = (CGFloat)_stepCount / 10000.0;
             _todayProgressView.progress = percent;
             
             if (percent < 0.60) {
-                _todayProgressView.progressTintColor = [UIColor colorWithRed:147.0/255.0 green:53.0/255.0 blue:53.0/255.0 alpha:0.7];
+                _todayProgressView.progressTintColor = [UIColor colorWithRed:255.0/255.0 green:59.0/255.0 blue:48.0/255.0 alpha:1.0];
             }
             else if (percent < 0.95) {
-                _todayProgressView.progressTintColor = [UIColor colorWithRed:255.0/255.0 green:225.0/255.0 blue:0.0/255.0 alpha:0.7];
+                _todayProgressView.progressTintColor = [UIColor colorWithRed:255.0/255.0 green:225.0/255.0 blue:0.0/255.0 alpha:1.0];
             }
             else {
-                _todayProgressView.progressTintColor = [UIColor colorWithRed:19.0/255.0 green:219.0/255.0 blue:33.0/255.0 alpha:0.7];
+                _todayProgressView.progressTintColor = [UIColor colorWithRed:76.0/255.0 green:217.0/255.0 blue:100.0/255.0 alpha:1.0];
             }
             
         });
@@ -131,7 +112,8 @@
 #pragma mark - Widget Handling
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
-    [self performSelectorOnMainThread:@selector(updateSteps) withObject:nil waitUntilDone:YES];
+    [[NSUserDefaults standardUserDefaults] setValue:@(_stepCount) forKey:@"stepCount"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
     completionHandler(NCUpdateResultNewData);
 }
 
